1a0802ecd75290aeb81668e341774c5839ad1ae5
Cam Michie <cameron.oscar.michie@gmail.com>
2024-08-23 10:39:00 +0100

    init commit

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,29 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have the 'ably-roomlevel-reactions-angular' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('ably-roomlevel-reactions-angular');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ably-roomlevel-reactions-angular');
+  });
+});
---
+++ src/main.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { ChatClient, RoomOptionsDefaults, Reaction } from '@ably/chat';
+import { Realtime } from 'ably';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: `
+    <h1>{{ name }}!</h1>
+    <p>Click these emojis to send a room-level reaction.</p>
+    <button type="button" id="smiley"><span> 😀 </span></button>
+    <button type="button" id="fire"><span> 🔥 </span></button>
+    <button type="button" id="heart"><span> ❤️ </span></button>
+    <button type="button" id="thumbsup"><span> 👍 </span></button>
+    <pre id="messages"></pre>
+  `,
+})
+export class App {
+  name = 'Room-level reactions using Ably Chat inside Angular';
+
+  constructor() {
+    this.initializeAbly();
+  }
+
+  async initializeAbly(): Promise<void> {
+    const clientId: string = Date.now().toString().slice(-5);
+    const ably = new Realtime({ key: 'Squ8ag.ex4hJg:Et2nm7jw0emMoDvrYbfeicLIZn9tYGUakJvIJ9slFoc', clientId: 'client-'+clientId });
+    const chat = new ChatClient(ably);
+
+    const room = chat.rooms.get('basketball-stream', {
+      reactions: RoomOptionsDefaults.reactions,
+    });
+
+    await room.attach();
+
+    const msgDisplay: HTMLElement | null = document.getElementById('messages');
+    if (msgDisplay) {
+      const { unsubscribe } = room.reactions.subscribe((reaction: Reaction) => {
+        const msgData: string = JSON.stringify(reaction, null, 2);
+        msgDisplay.textContent += `${msgData}\n`;
+      });
+
+      ['smiley', 'fire', 'heart', 'thumbsup'].forEach((id: string) => {
+        const button = document.getElementById(id);
+        if (button) {
+          button.addEventListener('click', async () => {
+            await room.reactions.send({ type: id });
+          });
+        }
+      });
+    }
+  }
+}
+
+bootstrapApplication(App);
